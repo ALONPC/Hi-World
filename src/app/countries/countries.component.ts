9e0867520b02d37add1a4af66278e001ed8477c8
AL_ON_PC <alon.berrios@gmail.com>
2018-11-27 13:49:49 -0300

    Finland and Sweden added; Changes made to the UI

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -15,9 +15,11 @@ export class CountriesComponent implements OnInit {
   countries : any = [];
   regions : any = [];
 
-  createCountry(name, territoryKm, arrRegions, code){
+  createCountry(name, capital, currency, territoryKm, arrRegions, code){
     var country =  new Country();
     country.name = name;
+    country.capital = capital;
+    country.currency = currency;
     country.territoryKm = territoryKm;
     country.regions = arrRegions;
     country.code = code;
@@ -41,17 +43,23 @@ export class CountriesComponent implements OnInit {
 
   constructor() { 
 
-    var australianStates = ['Western Australia','Northern Territory','South Australia','Queensland','New South Wales','Victoria'];
+    var australianStates    = ['Western Australia','Northern Territory','South Australia','Queensland','New South Wales','Victoria'];
     var cananadianProvinces = ['Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador', 'Northwest Territories', 'Nova Scotia', 'Nunavut', 'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan', 'Yukon Territory'];
-    var chileanRegions = ['Región de Arica y Parinacota','Región de Tarapacá','Región de Antofagasta','Región de Atacama', 'Región de Coquimbo', 'Región de Valparaíso', 'Región del Libertador General Bernardo OHiggins', 'Región del Maule', 'Región de Ñuble', 'Región del Bíobío', 'Región de la Araucanía', 'Región de los Ríos', 'Región de los Lagos', 'Región de Aysén del G. Carlos Ibañez del Campo', 'Región de Magallanes y de la Antártica Chilena', 'Región Metropolitana de Santiago'];
+    var chileanRegions      = ['Región de Arica y Parinacota','Región de Tarapacá','Región de Antofagasta','Región de Atacama', 'Región de Coquimbo', 'Región de Valparaíso', 'Región del Libertador General Bernardo OHiggins', 'Región del Maule', 'Región de Ñuble', 'Región del Bíobío', 'Región de la Araucanía', 'Región de los Ríos', 'Región de los Lagos', 'Región de Aysén del G. Carlos Ibañez del Campo', 'Región de Magallanes y de la Antártica Chilena', 'Región Metropolitana de Santiago'];
+    var swedishCounties     = ['Stockholm','Västerbotten','Norrbotten','Uppsala','Södermanland','Östergötland','Jönköping','Kronoberg','Kalmar','Gotland','Blekinge','Skåne','Halland','Västra Götaland','Värmland','Örebro','Västmanland','Dalarna','Gävleborg','Västernorrland','Jämtland'];
+    var finnishRegions      = ['Lapland','North Ostrobothnia','Kainuu','North Karelia','Pohjois-Savo','Etelä-Savo','South Ostrobothnia','Central Ostrobothnia','Ostrobothnia','Pirkanmaa','Central Finland','Satakunta','Varsinais-Suomi','South Karelia','Päijät-Häme','Kanta-Häme','Uusimaa','Kymenlaakso','Åland'];
 
-    var allAustralianRegions = this.buildRegionsOfCountry(australianStates);
-    var allCanadianProvinces = this.buildRegionsOfCountry(cananadianProvinces);
-    var allChileanRegions = this.buildRegionsOfCountry(chileanRegions);
+    var allAustralianRegions  = this.buildRegionsOfCountry(australianStates);
+    var allCanadianProvinces  = this.buildRegionsOfCountry(cananadianProvinces);
+    var allChileanRegions     = this.buildRegionsOfCountry(chileanRegions);
+    var allSwedishCounties    = this.buildRegionsOfCountry(swedishCounties);
+    var allFinnishRegions     = this.buildRegionsOfCountry(finnishRegions);
 
-    this.createCountry('Australia', 7741220, allAustralianRegions, 'au');
-    this.createCountry('Canada', 9984670, allCanadianProvinces, 'ca');
-    this.createCountry('Chile', 7561024, allChileanRegions, 'cl');
+    this.createCountry('Australia', 'Camberra', 'Australian Dollar', 7741220, allAustralianRegions, 'au');
+    this.createCountry('Canada', 'Ottawa', 'Canadian Dollar', 9984670, allCanadianProvinces, 'ca');
+    this.createCountry('Chile', 'Santiago', 'Chilean Peso', 7561024, allChileanRegions, 'cl');
+    this.createCountry('Sweden', 'Stockholm', 'Swedish Krona', 450295, allSwedishCounties, 'se');
+    this.createCountry('Finland', 'Helsinki', 'Euro', 338424, allFinnishRegions, 'fi');
 
     console.log(this.allRegions);
     console.log(this.allCountries);
--- src/classes/country.ts
+++ src/classes/country.ts
@@ -3,6 +3,8 @@ import { Region } from './region';
 export class Country {
 
     name: string;
+    capital : string;
+    currency: string;
     territoryKm: number;
     regions: Array<Region>;
     code: string;
