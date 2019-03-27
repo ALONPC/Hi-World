bcf9412c7181cd0b166ec2ed8d8793e70dd62732
ALONPC <alon.berrios@gmail.com>
2019-03-27 11:30:06 -0300

    Revisiting the project; Identation applied

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -9,14 +9,14 @@ import { Region } from '../../classes/region';
 })
 export class CountriesComponent implements OnInit {
 
-  allCountries : any = [];
-  allRegions : any = [];
+  allCountries: any = [];
+  allRegions: any = [];
 
-  countries : any = [];
-  regions : any = [];
+  countries: any = [];
+  regions: any = [];
 
-  createCountry(name, capital, languages, currency, nationalDay, territoryKm, arrRegions, code){
-    var country =  new Country();
+  createCountry(name, capital, languages, currency, nationalDay, territoryKm, arrRegions, code) {
+    const country =  new Country();
     country.name = name;
     country.capital = capital;
     country.languages = languages;
@@ -28,14 +28,14 @@ export class CountriesComponent implements OnInit {
     this.allCountries.push(country);
   }
 
-  createRegion(name){
-    var region = new Region();
+  createRegion(name) {
+    const region = new Region();
     region.name = name;
     return region;
   }
 
-  buildRegionsOfCountry(arrRegions){
-    var regionsOfCountry = [];
+  buildRegionsOfCountry(arrRegions) {
+    const regionsOfCountry = [];
     for (let i = 0; i < arrRegions.length; i++) {
       regionsOfCountry.push(this.createRegion(arrRegions[i]));
     }
@@ -44,47 +44,47 @@ export class CountriesComponent implements OnInit {
   }
 
   getGetOrdinal(n) {
-    var s=["th","st","nd","rd"],
-    v=n%100;
-    return n+(s[(v-20)%10]||s[v]||s[0]);
+    const s = ['th', 'st', 'nd', 'rd'],
+    v = n % 100;
+    return n + (s[(v - 20) % 10] || s[v] || s[0]);
   }
 
-  formatNationalDay(nationalDay){
-    var year : any;
-    var date = new Date(nationalDay);
-    var day = date.getDate();
-    var dayOrdinal = this.getGetOrdinal(day);
-    var month = date.toLocaleString('en-US', {
-      month:'long'
+  formatNationalDay(nationalDay) {
+    let year: any;
+    const date = new Date(nationalDay);
+    const day = date.getDate();
+    const dayOrdinal = this.getGetOrdinal(day);
+    const month = date.toLocaleString('en-US', {
+      month: 'long'
     });
-    if(date.getFullYear()!=2001){
+    if (date.getFullYear() !== 2001) {
       year = date.getFullYear();
     } else {
       year = '';
     }
-    var dateStr = month+', '+dayOrdinal+' '+year;
+    const dateStr = month + ', ' + dayOrdinal + ' ' + year;
     return dateStr;
   }
 
-  constructor() { 
+  constructor() {
 
-    var auLang = ['English'];
-    var caLang = ['English','French'];
-    var clLang = ['Chilean Flaite','Spanish'];
-    var seLang = ['Swedish'];
-    var fiLang = ['Finnish','Swedish'];
+    const auLang = ['English'];
+    const caLang = ['English', 'French'];
+    const clLang = ['Chilean Flaite', 'Spanish'];
+    const seLang = ['Swedish'];
+    const fiLang = ['Finnish', 'Swedish'];
 
-    var australianStates    = ['Western Australia','Northern Territory','South Australia','Queensland','New South Wales','Victoria'];
-    var cananadianProvinces = ['Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador', 'Northwest Territories', 'Nova Scotia', 'Nunavut', 'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan', 'Yukon Territory'];
-    var chileanRegions      = ['Región de Arica y Parinacota','Región de Tarapacá','Región de Antofagasta','Región de Atacama', 'Región de Coquimbo', 'Región de Valparaíso', 'Región del Libertador General Bernardo OHiggins', 'Región del Maule', 'Región de Ñuble', 'Región del Bíobío', 'Región de la Araucanía', 'Región de los Ríos', 'Región de los Lagos', 'Región de Aysén del G. Carlos Ibañez del Campo', 'Región de Magallanes y de la Antártica Chilena', 'Región Metropolitana de Santiago'];
-    var swedishCounties     = ['Stockholm','Västerbotten','Norrbotten','Uppsala','Södermanland','Östergötland','Jönköping','Kronoberg','Kalmar','Gotland','Blekinge','Skåne','Halland','Västra Götaland','Värmland','Örebro','Västmanland','Dalarna','Gävleborg','Västernorrland','Jämtland'];
-    var finnishRegions      = ['Lapland','North Ostrobothnia','Kainuu','North Karelia','Pohjois-Savo','Etelä-Savo','South Ostrobothnia','Central Ostrobothnia','Ostrobothnia','Pirkanmaa','Central Finland','Satakunta','Varsinais-Suomi','South Karelia','Päijät-Häme','Kanta-Häme','Uusimaa','Kymenlaakso','Åland'];
+    const australianStates    = ['Western Australia', 'Northern Territory', 'South Australia', 'Queensland', 'New South Wales', 'Victoria'];
+    const cananadianProvinces = ['Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador', 'Northwest Territories', 'Nova Scotia', 'Nunavut', 'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan', 'Yukon Territory'];
+    const chileanRegions      = ['Región de Arica y Parinacota', 'Región de Tarapacá', 'Región de Antofagasta', 'Región de Atacama', 'Región de Coquimbo', 'Región de Valparaíso', 'Región del Libertador General Bernardo OHiggins', 'Región del Maule', 'Región de Ñuble', 'Región del Bíobío', 'Región de la Araucanía', 'Región de los Ríos', 'Región de los Lagos', 'Región de Aysén del G. Carlos Ibañez del Campo', 'Región de Magallanes y de la Antártica Chilena', 'Región Metropolitana de Santiago'];
+    const swedishCounties     = ['Stockholm', 'Västerbotten', 'Norrbotten', 'Uppsala', 'Södermanland', 'Östergötland', 'Jönköping', 'Kronoberg', 'Kalmar', 'Gotland', 'Blekinge', 'Skåne', 'Halland', 'Västra Götaland', 'Värmland', 'Örebro', 'Västmanland', 'Dalarna', 'Gävleborg', 'Västernorrland', 'Jämtland'];
+    const finnishRegions      = ['Lapland', 'North Ostrobothnia', 'Kainuu', 'North Karelia', 'Pohjois-Savo', 'Etelä-Savo', 'South Ostrobothnia', 'Central Ostrobothnia', 'Ostrobothnia', 'Pirkanmaa', 'Central Finland', 'Satakunta', 'Varsinais-Suomi', 'South Karelia', 'Päijät-Häme', 'Kanta-Häme', 'Uusimaa', 'Kymenlaakso', 'Åland'];
 
-    var allAustralianRegions  = this.buildRegionsOfCountry(australianStates);
-    var allCanadianProvinces  = this.buildRegionsOfCountry(cananadianProvinces);
-    var allChileanRegions     = this.buildRegionsOfCountry(chileanRegions);
-    var allSwedishCounties    = this.buildRegionsOfCountry(swedishCounties);
-    var allFinnishRegions     = this.buildRegionsOfCountry(finnishRegions);
+    const allAustralianRegions  = this.buildRegionsOfCountry(australianStates);
+    const allCanadianProvinces  = this.buildRegionsOfCountry(cananadianProvinces);
+    const allChileanRegions     = this.buildRegionsOfCountry(chileanRegions);
+    const allSwedishCounties    = this.buildRegionsOfCountry(swedishCounties);
+    const allFinnishRegions     = this.buildRegionsOfCountry(finnishRegions);
 
     this.createCountry('Australia', 'Camberra', auLang, 'Australian Dollar', 'January-26', 7741220, allAustralianRegions, 'au');
     this.createCountry('Canada', 'Ottawa', caLang, 'Canadian Dollar', 'July-1', 9984670, allCanadianProvinces, 'ca');
@@ -98,7 +98,7 @@ export class CountriesComponent implements OnInit {
   }
 
   ngOnInit() {
-    
+
   }
 
 }
